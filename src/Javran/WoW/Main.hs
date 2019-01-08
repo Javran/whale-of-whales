21042367ba3b593b9171041c10cc46ed3fc4a885
Javran Cheng <Javran.C@gmail.com>
2019-01-08 12:32:56 -0800

    cleanup and test run

--- src/Javran/WoW/Main.hs
+++ src/Javran/WoW/Main.hs
@@ -1,6 +1,26 @@
+{-# LANGUAGE
+    OverloadedStrings
+  #-}
 module Javran.WoW.Main
   ( main
   ) where
 
+import Network.HTTP.Client (newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Web.Telegram.API.Bot
+import System.Environment
+import Data.String
+
+getToken :: IO Token
+getToken = Token . fromString <$> getEnv "BOT_TOKEN"
+
 main :: IO ()
-main = pure ()
+main = do
+  mgr <- newManager tlsManagerSettings
+  tok <- getToken
+  result <- runTelegramClient tok mgr $ do
+    info <- getWebhookInfoM
+    isSet <- setWebhookM (setWebhookRequest' "https://example.com/hook")
+    getMeM
+  print result
+  putStrLn "done"
