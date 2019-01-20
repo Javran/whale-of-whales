482641181200880f917e72b94ecf7e640aee76ce
Javran Cheng <Javran.c@gmail.com>
2019-01-20 00:40:02 -0800

    separate user verification module

--- src/Javran/Wow/BotModule/UserVerification.hs
+++ src/Javran/Wow/BotModule/UserVerification.hs
@@ -2,26 +2,39 @@
     LambdaCase
   , NamedFieldPuns
   , OverloadedStrings
+  , RecordWildCards
   #-}
 module Javran.Wow.BotModule.UserVerification
-  (
+  ( UserVerification
   ) where
 
-import Javran.Wow.Types
+import Data.Int
+  ( Int64
+  )
+import Control.Monad.RWS
+import Data.Maybe
+  ( fromMaybe
+  )
+import Data.String
+  ( fromString
+  )
+import Data.Time.Clock.POSIX
+  ( getCurrentTime
+  )
+import Data.Time
+  ( diffUTCTime
+  )
+import Data.Default.Class
 import Web.Telegram.API.Bot
+
 import qualified Data.Text as T
 import qualified Data.Map.Strict as M
-import qualified Data.Set as S
 import qualified Data.IntMap.Strict as IM
 import qualified Data.IntSet as IS
-import Control.Monad.RWS
-import Data.Time.Clock.POSIX
 
-import Data.Default.Class
-import Data.Maybe
-
-import Javran.Wow.Default ()
+import Javran.Wow.Types
 import Javran.Wow.Base
+import Javran.Wow.Default ()
 
 data UserVerification
 
@@ -37,58 +50,149 @@ modifyGroupState chatId f =
     f' Nothing = Just (f def)
     f' (Just x) = Just (f x)
 
+-- for every new chat member joining, we send out a message to initiate verification
+processNewMembers :: Int -> Int64 -> [User] -> WowM ()
 processNewMembers msgId chatId usersInp = do
-        let curGroupId = T.pack (show chatId)
-        GroupState {pendingKicks} <- getGroupState curGroupId
-            -- extract pending users (for this group)
-        let pendingUsers :: IS.IntSet
-            pendingUsers = foldMap userSet pendingKicks
-            shouldVerify user =
-              not (user_is_bot user) && not (user_id user `IS.member` pendingUsers)
+    let curGroupId = T.pack (show chatId)
+    GroupState {pendingKicks} <- getGroupState curGroupId
+    -- extract pending users (for this group)
+    let pendingUsers :: IS.IntSet
+        pendingUsers = foldMap userSet pendingKicks
         -- we should only verify non-bot users,
-        -- also if a user is in the process of verification,
-        -- we need to make sure re-entering does nothing.
-        case filter shouldVerify usersInp of
-          -- from this point on, we shouldn't use "usersInp" any more.
-          [] -> pure ()
-          users -> do
+        -- also we should ignore users already entered the pending kicks list
+        shouldVerify user =
+            not (user_is_bot user) && not (user_id user `IS.member` pendingUsers)
+    case filter shouldVerify usersInp of
+        -- from this point on, we shouldn't use "usersInp" any more.
+        [] -> pure ()
+        users -> do
             -- send message
-            let mk txt payload = def
-                                 { ikb_text = txt
-                                 , ikb_callback_data = Just payload
-                                 }
+            let mk txt payload =
+                  def { ikb_text = txt
+                      , ikb_callback_data = Just payload
+                      }
                 req = def
                       { message_chat_id = ChatId chatId
                       , message_text = "是萌妹子吗?"
                       , message_reply_to_message_id = Just msgId
-                      , message_reply_markup = Just (ReplyInlineKeyboardMarkup
-                                               [ [mk "是" "y"]
-                                               , [mk "不是" "n"]
-                                               , [mk "114514" "s"]
-                                               ])
+                      , message_reply_markup =
+                          Just (ReplyInlineKeyboardMarkup
+                                 [ [mk "是" "y"]
+                                 , [mk "不是" "n"]
+                                 , [mk "114514" "s"]
+                                 ])
                       }
             Response{result = Message {message_id = respMsgId}} <- liftTC $ sendMessageM req
             timestamp <- liftIO getCurrentTime
             let modifyKick gs@GroupState{pendingKicks = pks} =
                     gs {pendingKicks = IM.insert respMsgId uvm pks}
                   where
-                    uvm = UserVerificationMessage
-                          { timestamp
-                          , userSet = IS.fromList (user_id <$> users)
-                          }
+                    uvm =
+                      UserVerificationMessage
+                        { timestamp
+                        , userSet = IS.fromList (user_id <$> users)
+                        }
             modifyGroupState curGroupId modifyKick
 
+processUserAction :: CallbackQuery -> WowM ()
+processUserAction = \case
+    CallbackQuery
+        { cq_from = User {user_id, user_username}
+        , cq_message = Just Message {message_id, chat = Chat {chat_id}}
+        , cq_id
+        } -> do
+        let curChatId = T.pack (show chat_id)
+        groupState <- getGroupState curChatId
+        let GroupState {pendingKicks = pks} = groupState
+        case IM.lookup message_id pks of
+            Nothing -> pure ()
+            Just UserVerificationMessage {..} -> do
+                 let aReq = def { cq_callback_query_id = cq_id }
+                 _ <- tryWithTag "VerifAns" $ liftTC $ answerCallbackQueryM aReq
+                 when (user_id `IS.member` userSet) $ do
+                     -- user has completed verification
+                     -- remove record and say hi
+                     let modifyUVM x@UserVerificationMessage{userSet = us} =
+                           x {userSet = IS.delete user_id us}
+                         modifyKick :: GroupState -> GroupState
+                         modifyKick gs@GroupState {pendingKicks = pks'} =
+                           gs {pendingKicks = IM.adjust modifyUVM message_id pks'}
+                     modifyGroupState curChatId modifyKick
+                     let welcomeMsg = case user_username of
+                             Just u -> "欢迎" <> u <> "!"
+                             Nothing -> "欢迎" <> fromString (show user_id) <> "!"
+                         req = def { message_chat_id = ChatId chat_id
+                                   , message_text = welcomeMsg
+                                   }
+                     _ <- tryWithTag "VerifSayHi" $ liftTC $ sendMessageM req
+                     pure ()
+    _ -> pure ()
+
+-- kick timed out users, and try to delete the initial message if needed
+processKicks :: WowM ()
+processKicks = do
+    WEnv{kickTimeout} <- ask
+    curTime <- liftIO getCurrentTime
+    let timeExceeded UserVerificationMessage{timestamp} =
+            floor timeDiff > kickTimeout
+          where
+            timeDiff = curTime `diffUTCTime` timestamp
+    (WPState{groupStates = gss}, _) <- get
+    let processKicksByGroup :: T.Text -> GroupState -> WowM GroupState
+        processKicksByGroup groupId gs@GroupState{pendingKicks} = do
+          let (outdatedKicks, stillPendingKicks) = IM.partition timeExceeded pendingKicks
+              -- collect verif message ids
+              spToClear0 :: IS.IntSet
+              spToClear0 = IM.keysSet outdatedKicks
+              -- collect users that we want to kick out
+              kickingUsers = IS.toList $ foldMap userSet outdatedKicks
+          forM_ kickingUsers $ \userId ->
+            tryWithTag "KickAttempt" $
+              liftTC $ do
+                -- kick & unban, by doing so users are allowed to re-join
+                _ <- kickChatMemberM groupId userId
+                _ <- unbanChatMemberM groupId userId
+                pure ()
+          -- verif messge comes from two places:
+          -- oudated messages(0) + those that no longer contain users(1)
+          let (spToClearPre1, pendingKicks') = IM.partition isEmpty stillPendingKicks
+                where
+                  isEmpty u = IS.null (userSet u)
+              spToClear :: IS.IntSet
+              spToClear = spToClear0 `IS.union` IM.keysSet spToClearPre1
+          -- remove verif messages
+          _ <- tryWithTag "VerifCleanup" $ liftTC $
+            forM (IS.toList spToClear) $ \msgId -> do
+                let req = def
+                          { del_msg_chat_id = ChatId (read (T.unpack groupId))
+                          , del_msg_message_id = msgId
+                          }
+                deleteMessageM req
+          pure gs{pendingKicks = pendingKicks'}
+
+    gss' <- M.traverseWithKey processKicksByGroup gss
+    modify (\(s,gs) -> (s{groupStates = gss'}, gs))
 
 instance BotModule UserVerification where
-  bmUpdFulfiller _ = UpdFulfiller $ \case
-      Update
-        { message = Just Message
-          { chat = Chat {chat_type = ct, chat_id = ci}
-          , new_chat_members = Just users
-          , message_id
+    bmUpdFulfiller _ = UpdFulfiller $ \case
+        Update
+          { message = Just Message
+            { chat = Chat {chat_type = ct, chat_id = ci}
+            , new_chat_members = Just users
+            , message_id
+            }
           }
-        }
-        | ct == Group || ct == Supergroup -> do
-            processNewMembers message_id ci users
-            pure True
-      _ -> pure False
+          | ct == Group || ct == Supergroup -> do
+              -- step #1: initiate message
+              processNewMembers message_id ci users
+              pure True
+        Update { callback_query = Just cq} -> do
+              -- step #2: user action
+              -- user should click on any of those buttons
+              -- so that we can discharge from kicks
+              processUserAction cq
+              pure True
+        _ ->
+              pure False
+
+    bmPostProcess _ = processKicks
--- src/Javran/Wow/ProcessUpdate.hs
+++ src/Javran/Wow/ProcessUpdate.hs
@@ -6,17 +6,16 @@
   , ScopedTypeVariables
   , TypeApplications
   , LambdaCase
+  , ExistentialQuantification
   #-}
 module Javran.Wow.ProcessUpdate
   ( processUpdate
-  , processKicks
   , loadState
   , saveState
   , startBot
   ) where
 
 import Control.Monad.IO.Class
-import Data.String
 import Control.Monad.RWS
 import Web.Telegram.API.Bot
 import Data.Time
@@ -24,7 +23,6 @@ import Data.Char
 import qualified Data.Text as T
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
-import qualified Data.IntMap.Strict as IM
 import qualified Data.IntSet as IS
 import Data.Maybe
 import Data.Int
@@ -37,8 +35,17 @@ import Data.Default.Class
 import Data.Time.Clock.POSIX
 import Network.HTTP.Client (newManager)
 import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Data.Proxy
 
 import Text.ParserCombinators.ReadP hiding (get)
+import Javran.Wow.BotModule.UserVerification (UserVerification)
+
+data BMod = forall bm. BotModule bm => BMod (Proxy bm)
+
+botMods :: [BMod]
+botMods =
+    [ BMod (Proxy :: Proxy UserVerification)
+    ]
 
 int64ToT :: Int64 -> T.Text
 int64ToT = T.pack . show
@@ -94,7 +101,7 @@ startBot wenv@WEnv{..} = fix $ \r errCount ->
                       }
             Response {..} <- getUpdatesM req
             pure result)
-          processKicks
+          mapM_ (\(BMod bm) -> bmPostProcess bm) botMods
           (newSt, _) <- get
           when (oldSt /= newSt) saveState
 
@@ -167,7 +174,6 @@ processRepeater :: T.Text -> Update -> WowM ()
 processRepeater groupId upd
   | Just digest@(curTime, (_, rd)) <- updateToRepeatDigest upd
   = do
-
     window <- asks repeatWindow
     cd <- asks repeatCooldown
     let timeCut =
@@ -314,7 +320,9 @@ processUpdate upd@Update{..} = do
     shouldProcess <- asks $
       \WEnv{watchingGroups=wg} ->
         shouldProcessUpdate wg upd
-    when shouldProcess $
+    when shouldProcess $ do
+      -- first intercept by new bot modules
+      processed <- getUpdFulfiller (foldMap (\(BMod m) -> bmUpdFulfiller m) botMods) upd
       case upd of
         Update
           { message =
@@ -322,7 +330,7 @@ processUpdate upd@Update{..} = do
                 { chat = Chat {chat_id}
                 }
           }
-          | Just cmd <- extractBotCommand msg -> do
+          | Just cmd <- extractBotCommand msg, not processed -> do
             let sendWhale = do
                   WEnv {whaleStickers} <- ask
                   whaleSticker <- pickM whaleStickers
@@ -338,55 +346,12 @@ processUpdate upd@Update{..} = do
               "/n" -> processYorN False upd
               _ -> liftIO $ putStrLn $ "unrecognized command: " ++ T.unpack cmd
         Update
-          { callback_query =
-              Just CallbackQuery
-                { cq_from = User {user_id, user_username}
-                , cq_message = Just Message {message_id, chat = Chat {chat_id}}
-                , cq_id
-                }
-          } -> do
-           let curChatId = T.pack (show chat_id)
-           groupState <- getGroupState curChatId
-           let GroupState {pendingKicks = pks} = groupState
-           case IM.lookup message_id pks of
-             Nothing -> pure ()
-             Just UserVerificationMessage {..} -> do
-               let aReq = def { cq_callback_query_id = cq_id }
-               _ <- tryWithTag "VerifAns" $ liftTC $ answerCallbackQueryM aReq
-               when (user_id `IS.member` userSet) $ do
-                 -- user has completed verification
-                 -- remove record and say hi
-                 let modifyUVM x@UserVerificationMessage{userSet = us} =
-                       x {userSet = IS.delete user_id us}
-                     modifyKick :: GroupState -> GroupState
-                     modifyKick gs@GroupState {pendingKicks = pks'} =
-                       gs {pendingKicks = IM.adjust modifyUVM message_id pks'}
-                 modifyGroupState curChatId modifyKick
-                 let welcomeMsg = case user_username of
-                       Just u -> "欢迎" <> u <> "!"
-                       Nothing -> "欢迎" <> fromString (show user_id) <> "!"
-                     req = def { message_chat_id = ChatId chat_id
-                               , message_text = welcomeMsg
-                               }
-                 _ <- tryWithTag "VerifSayHi" $ liftTC $ sendMessageM req
-                 pure ()
-        Update
-          { message =
-              Just Message
-                { chat = Chat {chat_type = ct, chat_id = ci}
-                , new_chat_members = Just users
-                , message_id
-                }
-          }
-          | ct == Group || ct == Supergroup ->
-            processNewMembers message_id ci users
-        Update
           { message =
               Just Message
                 { sticker = Just Sticker {sticker_file_id}
                 , chat = Chat {chat_id}
                 }
-          } -> do
+          } | not processed -> do
               liftIO $ putStrLn $ "sticker received: " ++ show sticker_file_id
               liftIO $ putStrLn $ "[sticker] " ++ show upd
               processRepeater (int64ToT chat_id) upd
@@ -395,95 +360,10 @@ processUpdate upd@Update{..} = do
             Just Message
                  { chat = Chat {chat_id = ci}
                  }
-          } -> do
+          } | not processed -> do
           liftIO $ putStrLn $ "[msg] " ++ show upd
           processRepeater (int64ToT ci) upd
-        _ -> do
+        _ | not processed -> do
           let dbg = False
           when dbg $ liftIO $ putStrLn $ "-- ignored: revceived: " ++ show upd
-  where
-    processNewMembers msgId chatId usersInp = do
-        let curGroupId = T.pack (show chatId)
-        GroupState {pendingKicks} <- getGroupState curGroupId
-            -- extract pending users (for this group)
-        let pendingUsers :: IS.IntSet
-            pendingUsers = foldMap userSet pendingKicks
-            shouldVerify user =
-              not (user_is_bot user) && not (user_id user `IS.member` pendingUsers)
-        -- we should only verify non-bot users,
-        -- also if a user is in the process of verification,
-        -- we need to make sure re-entering does nothing.
-        case filter shouldVerify usersInp of
-          -- from this point on, we shouldn't use "usersInp" any more.
-          [] -> pure ()
-          users -> do
-            -- send message
-            let mk txt payload = def
-                                 { ikb_text = txt
-                                 , ikb_callback_data = Just payload
-                                 }
-                req = def
-                      { message_chat_id = ChatId chatId
-                      , message_text = "是萌妹子吗?"
-                      , message_reply_to_message_id = Just msgId
-                      , message_reply_markup = Just (ReplyInlineKeyboardMarkup
-                                               [ [mk "是" "y"]
-                                               , [mk "不是" "n"]
-                                               , [mk "114514" "s"]
-                                               ])
-                      }
-            Response{result = Message {message_id = respMsgId}} <- liftTC $ sendMessageM req
-            timestamp <- liftIO getCurrentTime
-            let modifyKick gs@GroupState{pendingKicks = pks} =
-                    gs {pendingKicks = IM.insert respMsgId uvm pks}
-                  where
-                    uvm = UserVerificationMessage
-                          { timestamp
-                          , userSet = IS.fromList (user_id <$> users)
-                          }
-            modifyGroupState curGroupId modifyKick
-
--- kick timed out users, and try to delete survey messages if needed
-processKicks :: WowM ()
-processKicks = do
-    WEnv{kickTimeout} <- ask
-    curTime <- liftIO getCurrentTime
-    let timeExceeded UserVerificationMessage{timestamp} =
-            floor timeDiff > kickTimeout
-          where
-            timeDiff = curTime `diffUTCTime` timestamp
-    (WPState{groupStates = gss}, _) <- get
-    let processKicksByGroup :: T.Text -> GroupState -> WowM GroupState
-        processKicksByGroup groupId gs@GroupState{pendingKicks} = do
-          let (outdatedKicks, stillPendingKicks) = IM.partition timeExceeded pendingKicks
-              -- collect verif message ids
-              spToClear0 :: IS.IntSet
-              spToClear0 = IM.keysSet outdatedKicks
-              -- collect users that we want to kick out
-              kickingUsers = IS.toList $ foldMap userSet outdatedKicks
-          forM_ kickingUsers $ \userId ->
-            tryWithTag "KickAttempt" $
-              liftTC $ do
-                -- kick & unban, by doing so users are allowed to re-join
-                _ <- kickChatMemberM groupId userId
-                _ <- unbanChatMemberM groupId userId
-                pure ()
-          -- verif messge comes from two places:
-          -- oudated messages(0) + those that no longer contain users(1)
-          let (spToClearPre1, pendingKicks') = IM.partition isEmpty stillPendingKicks
-                where
-                  isEmpty u = IS.null (userSet u)
-              spToClear :: IS.IntSet
-              spToClear = spToClear0 `IS.union` IM.keysSet spToClearPre1
-          -- remove verif messages
-          _ <- tryWithTag "VerifCleanup" $ liftTC $
-            forM (IS.toList spToClear) $ \msgId -> do
-                let req = def
-                          { del_msg_chat_id = ChatId (read (T.unpack groupId))
-                          , del_msg_message_id = msgId
-                          }
-                deleteMessageM req
-          pure gs{pendingKicks = pendingKicks'}
-
-    gss' <- M.traverseWithKey processKicksByGroup gss
-    modify (\(s,gs) -> (s{groupStates = gss'}, gs))
+        _ -> pure ()
