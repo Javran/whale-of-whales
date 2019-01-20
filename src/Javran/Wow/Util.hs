bd21e3b4f3e311dc5c0e994f36159b6ef4aa2837
Javran Cheng <Javran.c@gmail.com>
2019-01-20 14:54:33 -0800

    refined userDesc

--- src/Javran/Wow/Util.hs
+++ src/Javran/Wow/Util.hs
@@ -4,10 +4,13 @@
   #-}
 module Javran.Wow.Util
   ( extractBotCommand
+  , userDesc
   ) where
 
 import qualified Data.Text as T
 import Web.Telegram.API.Bot
+import Data.Maybe
+import Data.Char
 
 {-
 
@@ -26,3 +29,29 @@ extractBotCommand msg
         filter (\m -> me_type m == "bot_command") es
     = Just (T.toLower . T.take me_length . T.drop me_offset $ content)
     | otherwise = Nothing
+
+-- try to figure out how to describe this user in chat.
+userDesc :: User -> T.Text
+userDesc
+    User
+      { user_first_name
+      , user_last_name
+      , user_username
+      , user_id
+      } =
+    if T.all (\c -> isSpace c || not (isPrint c)) attempt
+      then
+        -- attempt #2: describe by username,
+        -- if it exists, it won't be empty (at least 5 characters)
+        fromMaybe
+          -- last resort: user id, which guarantees to exist
+          ("UID:" <> T.pack (show user_id))
+          user_username
+      else attempt
+  where
+    firstName = user_first_name
+    lastName = fromMaybe T.empty user_last_name
+    -- attempt #1: describe by first name then optionally last name
+    attempt = if T.length lastName > 0
+      then firstName <> " " <> lastName
+      else firstName
